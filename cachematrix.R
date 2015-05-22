ed11ac77de304d0a9e28b1666b4c719402cf8654
Ben Bryan <BBryan10@gmail.com>
2015-05-22 16:52:31 -0400

    R coding programming assignment 2 has been completed.
    The code prompts were used to create the 2 required functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is very similar to the vector example in the assignment description. The 
+## primary differences are creation of a matrix insetad of a vector, using language
+## that indicates seeking an inverse rather than a mean, and then using the second
+## function to look for and then solve for the inverse of the matrix.
 
-## Write a short comment describing this function
+## The first function "makeCacheMatrix" sets the four necessary functions in order to 
+## set the matrix in the cache.  There is no printed output; it is only to set up the 
+## second function.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        a <- NULL
+        set <- function(y) {
+                x <<-y
+                a <<- NULL
+        }
+        get <- function() x
+        set_inv <- function(inverse) a <<-inverse
+        get_inv <- function() a
+        list(get = get, set= set, get_inv = get_inv, set_inv = set_inv)
 }
 
 
-## Write a short comment describing this function
+## The second function "cacheSolve" looks first to see whether the inverse is available.
+## If not, the inverse is created using the function solve(). 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       a <- x$get_inv()
+       if(!is.null(a)) {
+               message("getting cached data")
+               return(a)
+       }
+       b <- x$get()
+       a <- solve(b, ...)
+       x$set_inv(a)
+       a
+               ## Return a matrix that is the inverse of 'x'
 }
